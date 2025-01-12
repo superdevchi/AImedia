715f6e23ee136c2e18b0b3927ace18d81b21b4b7
chibuikehenry <chibuikeoswaldhenrychime@gmail.com>
2025-01-12 15:36:12 -0800

    Initial Commit

---
+++ AImedia/AImediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AImediaApp.swift
+//  AImedia
+//
+//  Created by Chibuike  Henry on 2025-01-12.
+//
+
+import SwiftUI
+
+@main
+struct AImediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AImediaTests/AImediaTests.swift
@@ -0,0 +1,16 @@
+//
+//  AImediaTests.swift
+//  AImediaTests
+//
+//  Created by Chibuike  Henry on 2025-01-12.
+//
+
+import Testing
+
+struct AImediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
