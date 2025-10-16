25ee2edae2e0348d1388d5e015f7b33853d298aa
chiware <156154687+superdevchi@users.noreply.github.com>
2025-10-16 12:27:39 -0700

    Update AImediaApp.swift

--- AImedia/AImediaApp.swift
+++ AImedia/AImediaApp.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 @main
 struct AImediaApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+//delegate to clear core data on app termination
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Clear Core Data on termination
+        CoreDataManager.shared.clearAllData()
+        
+        
+        print("Core Data cleared on app termination")
+    }
+}
+
+
+
+
+
+
